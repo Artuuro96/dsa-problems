b691209aecb00d15f5f3afc3ae3c56ecebd773c7
Arturo Rodríguez <39638664+Artuuro96@users.noreply.github.com>
2023-09-28 19:10:47 -0500

    Update pow-number.ts size tab

--- arrays-and-hash/pow-number.ts
+++ arrays-and-hash/pow-number.ts
@@ -1,16 +1,16 @@
 let temp
 
 function main(a: number, b:number): any {
-	b = Math.floor(b)
-	if(b==0){
-		return 1;
-	}
-	temp = main(a, 	b/2);
-	if(b%2 != 0) {
-		return temp*temp*a
-	} else {
-		return temp*a
-	}
+  b = Math.floor(b)
+  if(b==0){
+    return 1;
+  }
+  temp = main(a, b/2);
+  if(b%2 != 0) {
+    return temp*temp*a
+  } else {
+    return temp*a
+  }
 }
 
 console.log("=====>", main(3,5));
